215dd35a9b87ec5b84661d8c38e7942cebd0c543
zbhdjdlgxy <zbhdjdlgxy@126.com>
2016-08-25 17:56:41 +0800

    ageaeag

---
+++ Project - 16 仿网易彩票客户端/Project - 16 仿网易彩票客户端/ZBHomeStatusesParam.swift
@@ -0,0 +1,15 @@
+//
+//  ZBHomeStatusesParam.swift
+//  Project - 16 仿网易彩票客户端
+//
+//  Created by 张标 on 16/8/18.
+//  Copyright © 2016年 szgyzb. All rights reserved.
+//
+
+import Foundation
+
+final class ZBHomeStatusesParam {
+    
+    var since_id : NSNumber?
+
+}
